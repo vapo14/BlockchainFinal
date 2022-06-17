a84052878fcc510245a35a83698a1f72e67af12c
GerardoGran <gerardogr.al@outlook.com>
2022-06-16 20:15:56 -0600

    comments

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.0 <0.9.0;
 
-// importar contrato helper con modificadores
-import "./BallotHelper.sol";
+import "./BallotHelper.sol"; /// importar contrato helper con modificadores
 
+/*
+ * @author Gerardo Granados y Victor Padron
+ * @dev Maneja votaciones aseguradas con direcciones de Ethereum
+ */
 contract Ballot is BallotHelper {
-    
-    // Variables 
     struct vote {
         address voterAddress;
         bool choice;
@@ -17,45 +18,45 @@ contract Ballot is BallotHelper {
         bool voted;
     }
 
-    uint private countResult = 0;   // Variable privada, solo se incrementa por voto
-    uint public finalResult = 0;    // Variable publica, solo se modifica al terminar la votacion
-    uint public totalVoter = 0;     // Personas que se han registrado
-    uint public totalVote = 0;      // Cuantas personas votaron a favor.
+    uint256 private countResult = 0; /// Variable privada, solo se incrementa por voto
+    uint256 public finalResult = 0; /// Variable publica, solo se modifica al terminar la votacion
+    uint256 public totalVoter = 0; /// Personas que se han registrado
+    uint256 public totalVote = 0; /// Cuantas personas votaron a favor.
 
+    mapping(uint256 => vote) private votes; /// mapa para registrar los votos, el numero de voto apunta al voto
+    mapping(address => voter) public voterRegister; /// mapa de registro de votos, la direccion apunta al que vota
 
+    /// evento que se ejecuta al terminar la votacion, registra el dueno, el nombre de la boleta, la propuesta a votar y el resultado final
+    event TerminateBallot(
+        address indexed _owner,
+        string _ballotOfficialName,
+        string _proposal,
+        uint256 _finalResult
+    );
 
-    mapping(uint => vote) private votes;            // mapa para registrar los votos, el numero de voto apunta al voto
-    mapping(address => voter) public voterRegister; // mapa de registro de votos, la direccion apunta al que vota
-
-
-    // Event
-    // evento que se ejecuta al terminar la votacion, registra el dueno, el nombre de la boleta, la propuesta a votar y el resultado final
-    event TerminateBallot(address indexed _owner, string _ballotOfficialName, string _proposal, uint _finalResult);
-
-    // Functions
     constructor(
-        // recibe el nombre boleta
+        /// recibe el nombre boleta
         string memory _ballotOfficialName,
-        // recibe la propuesta a votar
-        string memory _proposal        
-    ){
-        // el dueno oficial de la boleta
+        /// recibe la propuesta a votar
+        string memory _proposal
+    ) {
+        /// el dueno oficial de la boleta
         ballotOfficialAddress = msg.sender;
-        // el nombre oficial de la boleta
+        /// el nombre oficial de la boleta
         ballotOfficialName = _ballotOfficialName;
-        // la propuesta
+        /// la propuesta
         proposal = _proposal;
 
-
-        // el estado de la votacion
+        /// el estado de la votacion
         state = State.Created;
     }
 
-    /**
-        funcion de agregar votante
-        recibe la direccion 
+    /*
+     * @dev agrega votante que tiene permiso de votar.
+     * @param _voterAddress: address con la direccion del votante.
+     * @param _voterName: string con el nombre del votante.
      */
-    function addVoter(address _voterAddress, string memory _voterName) 
+    function addVoter(address _voterAddress, string memory _voterName)
         public
         inState(State.Created)
         onlyOfficial
@@ -64,32 +65,36 @@ contract Ballot is BallotHelper {
         v.voterName = _voterName;
         v.voted = false;
         voterRegister[_voterAddress] = v;
-        totalVoter++;
+        totalVoter++; /// Aumenta el total de votantes posibles.
     }
 
-    function startVote()
-        public
-        inState(State.Created)
-        onlyOfficial
-    {
+    /*
+     * @dev Cambia el estado a Voting para empezar a coleccionar votos.
+     */
+    function startVote() public inState(State.Created) onlyOfficial {
         state = State.Voting;
-
     }
 
+    /*
+     * @dev Confirma que el votante puede votar, registra su voto y aumenta los votos ejecutados.
+     * @param _choice: bool con la decision del votante.
+     */
     function doVote(bool _choice)
         public
         inState(State.Voting)
-        returns (bool voted) 
+        returns (bool voted)
     {
         bool found = false;
-        // if the voter exists in the registry and has not voted then dovote
-        if (bytes(voterRegister[msg.sender].voterName).length != 0 
-        && !voterRegister[msg.sender].voted) {
+        /// if the voter exists in the registry and has not voted then dovote
+        if (
+            bytes(voterRegister[msg.sender].voterName).length != 0 &&
+            !voterRegister[msg.sender].voted
+        ) {
             voterRegister[msg.sender].voted = true;
             vote memory v;
             v.voterAddress = msg.sender;
             v.choice = _choice;
-            if(_choice){
+            if (_choice) {
                 countResult++;
             }
             votes[totalVote] = v;
@@ -99,14 +104,17 @@ contract Ballot is BallotHelper {
         return found;
     }
 
-    function endVote()
-        public
-        inState(State.Voting)
-        onlyOfficial
-    {
+    /*
+     * @dev Cambia el estado a Ended para dejar de coleccionar votos.
+     */
+    function endVote() public inState(State.Voting) onlyOfficial {
         state = State.Ended;
         finalResult = countResult;
-        emit TerminateBallot(msg.sender, ballotOfficialName, proposal, finalResult);
+        emit TerminateBallot(
+            msg.sender,
+            ballotOfficialName,
+            proposal,
+            finalResult
+        );
     }
-
 }
--- contracts/BallotHelper.sol
+++ contracts/BallotHelper.sol
@@ -1,27 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.0 <0.9.0;
 
+/*
+ * @author Gerardo Granados y Victor Padron
+ * @dev Clase padre con informacion de las votaciones.
+ */
 contract BallotHelper {
+    enum State {
+        Created,
+        Voting,
+        Ended
+    } /// Para ver el estado de la votacion actual
+    address public ballotOfficialAddress; /// Para reconocer al oficial
+    string public ballotOfficialName; /// Para identificar al oficial
+    string public proposal; /// Para qué se vota
 
-    enum State { Created, Voting, Ended }   // Para ver el estado de la votacion actual
-    address public ballotOfficialAddress;   // Para reconocer al oficial
-    string public ballotOfficialName;       // Para identificar al oficial
-    string public proposal;                 // Para qué se vota
-    
     State public state;
 
-    // Modifiers
-    modifier condition(bool _condition) {   // Cumple una condicion
+    /// Modifiers
+    modifier condition(bool _condition) {
+        /// Cumple una condicion
         require(_condition);
         _;
     }
 
-    modifier onlyOfficial() {   // Solo el oficial
+    modifier onlyOfficial() {
+        /// Solo el oficial
         require(msg.sender == ballotOfficialAddress);
         _;
     }
 
-    modifier inState(State _state) {    // Para confirmar desde el enum de Stat que este en creado, en proceso, o terminado.
+    modifier inState(State _state) {
+        /// Para confirmar desde el enum de Stat que este en creado, en proceso, o terminado.
         require(state == _state);
         _;
     }
